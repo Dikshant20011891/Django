7b1672bdf81cd131a9a365d473a7bdd752638141
dikshant <dikshantjoshi20011891@gmail.com>
2024-09-09 14:42:26 +0530

    fir

---
+++ shop/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def index(request):
+    return render(request,'shop/index.html')
+
+def about(request):
+    return HttpResponse("Shop about")
+
+def contact(request):
+    return HttpResponse("Shop Con")
+
+def tracker(request):
+    return HttpResponse("Shop tra")
+
+def search(request):
+    return HttpResponse("Shop sea")
+
+def prodView(request):
+    return HttpResponse("Shop prod view")
+
+def checkOut(request):
+    return HttpResponse("Shop check")
