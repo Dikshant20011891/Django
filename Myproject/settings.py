9a6f57e3d83f268f836fff4a58f11604a6d5acab
dikshant <dikshantjoshi20011891@gmail.com>
2024-10-21 12:53:14 +0530

    fimnal

--- Myproject/settings.py
+++ Myproject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['Myproject/templates'], # Add this line
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
